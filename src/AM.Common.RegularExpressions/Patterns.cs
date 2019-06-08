dad5526037f89a31623594a49a83a93ab482344c
Artak Mkrtchyan <artak@outlook.com>
2019-06-08 09:05:55 -0700

    Changing the values from consts to reasonly strings to allow updated patterns be referenced by projects without recompilation

--- src/AM.Common.RegularExpressions/Patterns.cs
+++ src/AM.Common.RegularExpressions/Patterns.cs
@@ -2,10 +2,10 @@
 {
     public static class Patterns
     {
-        public const string WholeNumber = "^[0-9]+$";
+        public static readonly string WholeNumber = "^[0-9]+$";
 
-        public const string EmailAddress = @"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
+        public static readonly string EmailAddress = @"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
 
-        public const string Url = @"^(?:(?:https?|ftp):\/\/)?(?:[a-zA-Z0-9_\-]+(?:\.[a-zA-Z0-9]+)+)(?:\/[a-zA-Z0-9#]+[a-zA-Z0-9\-\.]*[a-zA-Z0-9]*)*(?:\?(?:(?:[a-zA-Z0-9_]+(?:[a-zA-Z0-9\+\%]*[a-zA-Z0-9_]+)?)\=(?:[a-zA-Z0-9_\-]+(?:[a-zA-Z0-9\+\%]*[a-zA-Z0-9_\+]+)?)*\&?)+)?\/?$";
+        public static readonly string Url = @"^(?:(?:https?|ftp):\/\/)?(?:[a-zA-Z0-9_\-]+(?:\.[a-zA-Z0-9]+)+)(?:\/[a-zA-Z0-9#]+[a-zA-Z0-9\-\.]*[a-zA-Z0-9]*)*(?:\?(?:(?:[a-zA-Z0-9_]+(?:[a-zA-Z0-9\+\%]*[a-zA-Z0-9_]+)?)\=(?:[a-zA-Z0-9_\-]+(?:[a-zA-Z0-9\+\%]*[a-zA-Z0-9_\+]+)?)*\&?)+)?\/?$";
     }
 }
